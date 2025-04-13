b88d23fd4e0b9874215a3a02b6d27b2e308c0b51
vandana-Manojkumar <manojvandana89@gmail.com>
2025-04-13 11:59:54 +0530

    changed cors url

--- server-main/server-main/index.js
+++ server-main/server-main/index.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 
 app.use(cors({
-  origin: 'https://find-jobs-chi.vercel.app/', 
+  origin: 'https://find-jobs-chi.vercel.app', 
   credentials: true 
 }));
 
